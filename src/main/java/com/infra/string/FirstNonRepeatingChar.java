f44af4ba0aece698a966484efebda38975015a57
KEVAL CHHEDA <kevalchheda.kc123@gmail.com>
2026-03-24 23:59:14 +0530

    find first non repeating character
    find first non repeating character

---
+++ src/main/java/com/infra/string/FirstNonRepeatingChar.java
@@ -0,0 +1,27 @@
+package com.infra.string;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+        String input = "programming";
+
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        // Count frequency
+        for (char c : input.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Find first non-repeating
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeating character: " + entry.getKey());
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character found");
+    }
+}
